115ad58552ff80c518c4df89709840a98d542264
Mohammad Mostajeran <mostajeran.mohammad.1999@gmail.com>
2024-01-06 09:32:21 +0330

    document NashvilleGrpcClientService

--- src/tasks/providers/nashville-grpc-client.service.ts
+++ src/tasks/providers/nashville-grpc-client.service.ts
@@ -12,6 +12,10 @@ import { CreateTaskDto } from '../dto/create-task.dto';
 import { UpdateTaskDto } from '../dto/update-task.dto';
 
 @Injectable()
+/**
+ * NashvilleGrpcClientService is a service that interacts with the gRPC client for the TasksService.
+ * It provides methods for creating, updating, deleting, and retrieving tasks.
+ */
 export class NashvilleGrpcClientService implements OnModuleInit {
   constructor(@Inject('TasksService') private readonly client: ClientGrpc) {}
   private tasksGrpcService: TasksService;
@@ -21,6 +25,11 @@ export class NashvilleGrpcClientService implements OnModuleInit {
       this.client.getService<TasksService>('TasksService');
   }
 
+  /**
+   * Creates a new task.
+   * @param body - The data for creating the task.
+   * @returns An Observable that emits the response of creating the task.
+   */
   createTask(body: CreateTaskDto): Observable<CreateTaskResponse> {
     return this.tasksGrpcService.CreateTask({
       title: body.title,
@@ -29,6 +38,12 @@ export class NashvilleGrpcClientService implements OnModuleInit {
     });
   }
 
+  /**
+   * Updates an existing task.
+   * @param taskId - The ID of the task to update.
+   * @param body - The data for updating the task.
+   * @returns An Observable that emits the response of updating the task.
+   */
   updateTask(
     taskId: string,
     body: UpdateTaskDto,
@@ -41,12 +56,23 @@ export class NashvilleGrpcClientService implements OnModuleInit {
     });
   }
 
+  /**
+   * Deletes a task.
+   * @param taskId - The ID of the task to delete.
+   * @returns An Observable that emits the response of deleting the task.
+   */
   deleteTask(taskId: string): Observable<DeleteTaskResponse> {
     return this.tasksGrpcService.DeleteTask({
       id: taskId,
     });
   }
 
+  /**
+   * Retrieves all tasks.
+   * @param pageSize - The number of tasks to retrieve per page.
+   * @param page - The page number of tasks to retrieve.
+   * @returns An Observable that emits the response of retrieving all tasks.
+   */
   getAllTasks(pageSize: string, page: string): Observable<GetAllTasksResponse> {
     return this.tasksGrpcService.GetAllTasks({
       pageNumber: parseInt(page, 10) || 1,
